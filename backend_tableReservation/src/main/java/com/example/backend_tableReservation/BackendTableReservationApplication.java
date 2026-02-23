c20440b409c088f916e0e943a2247081ec65686f
Tiisler <martentiisler@gmail.com>
2026-02-23 14:59:50 +0200

    Esialgne commit

---
+++ backend_tableReservation/src/main/java/com/example/backend_tableReservation/BackendTableReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_tableReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTableReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTableReservationApplication.class, args);
+	}
+
+}
---
+++ backend_tableReservation/src/test/java/com/example/backend_tableReservation/BackendTableReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_tableReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTableReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
